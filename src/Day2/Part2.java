e37055b1b25ca2489d78b0a332778662e6fbef15
Anna Kirillova <a.kirillova1812@gmail.com>
2022-12-02 12:07:30 +0400

    Part 1 and part 2 of the task1

---
+++ src/Day2/Part2.java
@@ -0,0 +1,55 @@
+package Day2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Part2 {
+	public static void main(String[] args) throws IOException {
+
+		String path = ".\\src\\Day2\\data.txt";
+		
+		final String [] strategy = Files.readString(Path.of(path)).split("\r\n");
+		
+		int totalScore = 0;
+
+		for (String val : strategy) {
+			String[] round = val.split(" ");
+			switch (round[0]) {
+			case "A":
+				if (round[1].equals("X")) {		//lost
+					totalScore += 3;
+				} else if (round[1].equals("Y")) {		//draw 
+					totalScore += (1 + 3);
+				} else {						// win
+					totalScore += (2 + 6);
+				}
+				break;
+			case "B":
+				if (round[1].equals("Y")) {		//draw
+					totalScore += (2 + 3);
+				} else if (round[1].equals("Z")) {		// win
+					totalScore += (3 + 6);
+				} else {						// lost
+					totalScore += 1;
+				}
+				break;
+			case "C":
+				if (round[1].equals("Z")) {		//win
+					totalScore += (1 + 6);
+				} else if (round[1].equals("X")) {		// lost
+					totalScore += 2;
+				} else {						// draw
+					totalScore += (3 + 3);
+				}
+				break;
+			default:
+				System.out.println("Error1!");
+				return;
+			}
+		}
+		
+		System.out.println(totalScore);
+		
+	}
+}
